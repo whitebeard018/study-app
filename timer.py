b79e78dd50fcc80a3d4e13afcef948fc96021901
rashishsaini <rashishsaini07@gmail.com>
2025-10-11 01:43:20 +0530

    add pomodoro timer outline

---
+++ timer.py
@@ -0,0 +1,26 @@
+import time 
+from plyer import notification
+
+WORK_TIME = 25 * 60   # 25 minutes
+BREAK_TIME = 5 * 60   # 5 minutes
+
+class PomodoroTimer:
+    def __init__(self):
+        self.session_count = 0
+
+    def start_work_session(self):
+        print("Work session started.")
+        time.sleep(WORK_TIME)
+        self.session_count += 1
+        notification.notify(title="Time's up!", message="Take a break!")
+
+    def start_break(self):
+        print("Break started.")
+        time.sleep(BREAK_TIME)
+        notification.notify(title="Break over!", message="Get back to work!")
+
+if __name__ == "__main__":
+    timer = PomodoroTimer()
+    while True:
+        timer.start_work_session()
+        timer.start_break()
